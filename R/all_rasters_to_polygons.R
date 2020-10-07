9ce7870c2961c79c55ee7f63c28c25ef1143198b
hrvg <herve@guillon.xyz>
2020-10-07 16:36:44 -0700

    Added a reversed option in all_rasters_to_polygons

--- R/all_rasters_to_polygons.R
+++ R/all_rasters_to_polygons.R
@@ -1,18 +1,18 @@
-#' This function transforms all rasters found into a directory into SpatialPolygons and save them to shapefiles.
+#' This function transforms all rasters found into a directory into SpatVector and save them to shapefiles.
 #' 
 #' The main use of this function is to parallelize processing for each pixels of each rasters.
 #' 
 #' @param input_dir `character`, input directory
 #' @param output_dir `character`, output directory
 #' @param reference_path optional, `character`, path to a reference raster used to crop the rasters found in `input_dir`
-#' @return `list` of `SpatialPolygons
+#' @param reverse `logical`, optional, if `TRUE` the files are processed in reverse order
+#' @return `list` of `SpatVector`
 #' @import foreach
 #' @import doFuture
-#' @import raster
 #' @import sp
 #' @import rgdal
 #' @export
-all_rasters_to_polygons <- function(input_dir, output_dir, reference_path = NULL){
+all_rasters_to_polygons <- function(input_dir, output_dir, reference_path = NULL, reversed = FALSE){
 	supported_ext <- c("tif", "grd")
 	raster_file <- NULL
 	if(!class(input_dir) == "character") stop("invalid class: input_dir is not of class 'character'")
@@ -31,12 +31,13 @@ all_rasters_to_polygons <- function(input_dir, output_dir, reference_path = NULL
 	file_list_ext <- sapply(file_list, tools::file_ext)
 	file_list <- file_list[which(file_list_ext %in% supported_ext)]
 	if(length(file_list) == 0) stop(paste("No supported file found at:", input_dir))
-	if(!is.null(reference_path)) reference_raster <- raster::raster(reference_path)
+	if(!is.null(reference_path)) reference_raster <- terra::rast(reference_path)
+	if (reversed) file_list <- rev(file_list)
 	all_polygons <- foreach(raster_file = file_list, .packages = c("raster", "sp", "rgdal")) %do% {
-		r <- raster::raster(raster_file)
-		if(!is.null(reference_path)) r <- raster::crop(r, reference_raster)
-		pol <- raster::rasterToPolygons(r, na.rm = FALSE)
-		raster::shapefile(pol, filename = file.path(output_dir, paste0(tools::file_path_sans_ext(basename(raster_file)), ".shp")), overwrite = TRUE)
+		r <- terra::rast(raster_file)
+		if(!is.null(reference_path)) r <- terra::crop(r, reference_raster)
+		pol <- terra::as.polygons(r[[2]], dissolve = FALSE)
+		terra::writeVector(pol, filename = file.path(output_dir, paste0(tools::file_path_sans_ext(basename(raster_file)), ".shp")), overwrite = TRUE)
 		return(pol)
 	}
 	return(all_polygons)
