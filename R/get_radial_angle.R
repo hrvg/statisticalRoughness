12e1230458ae07521f03d6bd285e3b9341d0409b
hrvg <herve@guillon.xyz>
2021-05-04 07:51:42 -0700

    trying out new angle selection

---
+++ R/get_radial_angle.R
@@ -0,0 +1,43 @@
+#' Wrapper function to derive the anisotropy exponent from a non-rotated raster
+#' @param rstr `Raster` object
+#' @param raster_resolution `numeric` the resolution of `rstr` in meters
+#' @param angle_step `numeric`, angular step
+#' @importFrom magrittr %>%
+#' @importFrom rlang .data
+#' @return a `data.frame`
+#' @export
+#' @keywords zeta
+get_radial_angle <- function(rstr, raster_resolution, angle_step){
+	angles <- seq(0, 90 - angle_step, angle_step)
+	res <- foreach(rotation_angle = angles, .combine = cbind) %do% {
+		rotated_raster <- rotate_raster(rstr, rotation_angle)
+		hhcf_x <- get_hhcf_(rotated_raster, raster_resolution, margin = 1, average = TRUE)
+		hhcf_y <- get_hhcf_(rotated_raster, raster_resolution, margin = 2, average = TRUE)
+		theta_x <- rotation_angle %% 360
+		theta_y <- (theta_x + 90) %% 360
+		alpha_x <- get_all_alpha_(hhcf_x, raster_resolution) 
+		alpha_y <- get_all_alpha_(hhcf_y, raster_resolution)
+		colnames(alpha_x) <- paste0(colnames(alpha_x), ".x")
+		colnames(alpha_y) <- paste0(colnames(alpha_y), ".y")
+		res <- dplyr::bind_cols(alpha_x, alpha_y)
+		res <- res %>%
+			dplyr::select(
+				.data$alpha1.x,
+				.data$alpha1.y
+			)
+		colnames(res) <- gsub(".x", paste0(".", theta_x), colnames(res))
+		colnames(res) <- gsub(".y", paste0(".", theta_y), colnames(res))
+		return(res)
+	}
+	alpha1 <- res %>% dplyr::select(dplyr::contains("alpha1.")) %>% as.matrix() %>% c()
+	alpha1_median <- stats::median(alpha1, na.rm = TRUE)
+	alpha1_mad <- stats::mad(alpha1, constant = 1, na.rm = TRUE)
+	theta <- res %>% dplyr::select(dplyr::contains("alpha1.")) %>% colnames() %>% gsub("alpha1.", "", .) %>% as.numeric()
+	theta <- theta[!is.na(alpha1)]
+	alpha1 <- alpha1[!is.na(alpha1)]
+	ww <- alpha1
+	ww <- ww / sum(ww)
+	dens <- density(theta, weights = ww, bw = 2 * angle_step)
+	theta_perp <- dens$x[which.max(dens$y)] %% 180
+	return(list(alpha1_median = alpha1_median, alpha1_mad = alpha1_mad, theta_perp = theta_perp))
+}
--- R/get_zeta.R
+++ R/get_zeta.R
@@ -62,7 +62,7 @@ get_kruskal_flag <- function(alpha_x, alpha_y, var){
 #' @return a `data.frame`
 #' @export
 #' @keywords zeta
-get_zeta <- function(rstr, raster_resolution, .mode = "radial", angle_step = 5, nbin = 20, .Hann = TRUE, .quantile_prob = c(0.99), .prob = .999, full = FALSE){
+get_zeta <- function(rstr, raster_resolution, .mode = "radial", angle_step = 1, nbin = 20, .Hann = TRUE, .quantile_prob = c(0.99), .prob = .999, full = FALSE){
 	if(.mode == "fourier"){
 		res <- get_zeta_fourier(rstr, raster_resolution, nbin, .Hann, .quantile_prob, .prob, full)
 	} else if(.mode == "radial"){
@@ -176,9 +176,10 @@ get_zeta_fourier <- function(rstr, raster_resolution, nbin, .Hann, .quantile_pro
 #' @export
 #' @keywords zeta
 get_zeta_radial <- function(rstr, raster_resolution, angle_step, full){
+	rstr <- rstr %>% raster::trim()
 	ext <- raster::extent(rstr)
 	circle <- sf::st_sfc(sf::st_buffer(sf::st_point(c(ext[1]+(ext[2]-ext[1])/2, ext[3]+(ext[4]-ext[3])/2)), mean(dim(rstr)[1:2]) * mean(raster::res(rstr)) / 2), crs = sf::st_crs(rstr)) %>% as("Spatial")
-	rstr <- raster::mask(rstr %>% raster::trim(), circle)
+	rstr <- raster::mask(rstr, circle)
 	radial_res <- get_radial_angle(rstr, raster_resolution, angle_step)
 	rotation_angle <- radial_res$theta_perp
 	rotated_raster <- rotate_raster(rstr, rotation_angle)
@@ -249,50 +250,4 @@ get_zeta_radial <- function(rstr, raster_resolution, angle_step, full){
 			.data$w.y
 		)
 	return(res)
-}
-
-#' Wrapper function to derive the anisotropy exponent from a non-rotated raster
-#' @param rstr `Raster` object
-#' @param raster_resolution `numeric` the resolution of `rstr` in meters
-#' @param angle_step `numeric`, angular step
-#' @importFrom magrittr %>%
-#' @importFrom rlang .data
-#' @return a `data.frame`
-#' @export
-#' @keywords zeta
-get_radial_angle <- function(rstr, raster_resolution, angle_step){
-	angles <- seq(0, 90 - angle_step, angle_step)
-	res <- foreach(rotation_angle = angles, .combine = cbind) %do% {
-		rotated_raster <- rotate_raster(rstr, rotation_angle)
-		hhcf_x <- get_hhcf_(rotated_raster, raster_resolution, margin = 1, average = TRUE)
-		hhcf_y <- get_hhcf_(rotated_raster, raster_resolution, margin = 2, average = TRUE)
-		theta_x <- rotation_angle %% 360
-		theta_y <- (theta_x + 90) %% 360
-		alpha_x <- get_all_alpha_(hhcf_x, raster_resolution) 
-		alpha_y <- get_all_alpha_(hhcf_y, raster_resolution)
-		colnames(alpha_x) <- paste0(colnames(alpha_x), ".x")
-		colnames(alpha_y) <- paste0(colnames(alpha_y), ".y")
-		res <- dplyr::bind_cols(alpha_x, alpha_y)
-		res <- res %>%
-			dplyr::select(
-				.data$alpha1.x,
-				.data$alpha1.y
-			)
-		colnames(res) <- gsub(".x", paste0(".", theta_x), colnames(res))
-		colnames(res) <- gsub(".y", paste0(".", theta_y), colnames(res))
-		return(res)
-	}
-	alpha1 <- res %>% dplyr::select(dplyr::contains("alpha1.")) %>% as.matrix() %>% c()
-	alpha1_median <- stats::median(alpha1, na.rm = TRUE)
-	alpha1_mad <- stats::mad(alpha1, constant = 1, na.rm = TRUE)
-	theta <- res %>% dplyr::select(dplyr::contains("alpha1.")) %>% colnames() %>% gsub("alpha1.", "", .) %>% as.numeric()
-	theta <- theta[!is.na(alpha1)]
-	alpha1 <- alpha1[!is.na(alpha1)]
-	alpha1 <- c(alpha1, alpha1)
-	theta <- c(theta, theta %>% +(180))
-	ww <- scales::rescale(alpha1)
-	ww <- ww / sum(ww)
-	circular_density <- spatstat.core::circdensity(theta, weights = ww, bw = angle_step)
-	theta_perp <- circular_density$x[which.max(circular_density$y)] %% 180
-	return(list(alpha1_median = alpha1_median, alpha1_mad = alpha1_mad, theta_perp = theta_perp))
 }
