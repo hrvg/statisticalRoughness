181f9e8d0f046f5ae6f5699812967ed89d4620e1
hrvg <herve@guillon.xyz>
2021-06-29 15:07:18 -0700

    added progressr back

--- Rscript/main_process_zeta.R
+++ Rscript/main_process_zeta.R
@@ -8,6 +8,13 @@ root_dir <- '/home/hguillon/research'
 crs_ref <- raster::crs("+proj=aea +lat_1=34 +lat_2=40.5 +lat_0=0 +lon_0=-120 +x_0=0 +y_0=-4000000 +ellps=GRS80 + +towgs84=0,0,0,0,0,0,0 +units=m +no_defs ")
 options(future.globals.maxSize= 2 * 1024^3) # 2 GiB
 
+
+if (as.numeric(R.version$major) >= 4) {
+	progressr::handlers(global = TRUE)
+	progressr::handlers("progress")
+}
+
+
 l <- 6
 n <- 30
 
